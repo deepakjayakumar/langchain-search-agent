5c3788aed7fd514145916b177ead135164688f23
deepakjayakumar <deepakjayakumar11@gmail.com>
2026-01-28 18:47:48 +0530

    including output parsing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from langchain_classic import hub
@@ -6,25 +7,40 @@ from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import Source,AgentResponse
 
 tools = [TavilySearch()]
 llm = ChatGoogleGenerativeAI(model="gemini-3-flash-preview")
 react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input","agent_scratchpad","tool_names"]
+).partial(format_instructions=output_parser.get_format_instructions)
+
 react_prompt.template += "\nVERY IMPORTANT: You must follow the format strictly. Do not include any text outside of the Thought/Action/Action Input/Observation blocks."
-agent = create_react_agent(
-    llm=llm,
-    tools=tools,
-    prompt=react_prompt
-)
+agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt_with_format_instructions)
 
 
-agent_executor = AgentExecutor(agent=agent,tools=tools,verbose=True )
-chain = agent_executor
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, 
+    handle_parsing_errors=True)
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+
+
+chain = agent_executor | extract_output | parse_output
+
 
 def main():
     result = chain.invoke(
         input={
-            "input":"search for 3 job posting for an ai engineer using langchain in chennai or bengaluru and list their details"
+            "input": "search for 3 job posting for an ai engineer using langchain in chennai or bengaluru and list their details"
         }
     )
     print(result)
